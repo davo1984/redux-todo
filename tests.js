f3df61b47154d6168b0722eb0cf414dfe180a135
David Oliver <davooliver1964@gmail.com>
2020-07-02 15:25:36 -0400

    convert from stand-alone to React

---
+++ tests.js
@@ -0,0 +1,28 @@
+    const toggleTodo = (todo) => {
+        return {
+            ...todo,
+            completed: !todo.completed
+        };
+    };
+
+    const testToggleTodo = () => {
+        const todoBefore = {
+            id: 0,
+            text: 'Learning Redux',
+            completed: false
+        };
+        const todoAfter = {
+            id: 0,
+            text: 'Learning Redux',
+            completed: true
+        };
+
+        deepFreeze(todoBefore);
+
+        expect(
+            toggleTodo(todoBefore)
+        ).toEqual(todoAfter);
+    };
+
+    testToggleTodo();
+    console.log('All tests passed.');
