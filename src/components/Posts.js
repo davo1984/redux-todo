3f06d6b341538f0577b5428b4578fb92c6dd1ea0
David Oliver <davooliver1964@gmail.com>
2020-07-05 22:07:29 -0400

    basic redux working

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,20 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
-export default class Posts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
-        }
-    }
+class Posts extends Component {
     componentWillMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res => res.json())
-            .then(data => this.setState({ posts: data }));
-        }
+        this.props.fetchPosts();
+    }
+
     render() {
-        {console.log(this.state.posts)}
-        const postItems = this.state.posts.map(post => (
+        const postItems = this.props.posts.map(post => (
             <div key={post.id}>
                 <h3>{post.title}</h3>
                 <p>{post.body}</p>
@@ -28,3 +22,9 @@ export default class Posts extends Component {
         )
     }
 }
+
+const mapStateToProps = state => ({
+    posts: state.posts.items
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
