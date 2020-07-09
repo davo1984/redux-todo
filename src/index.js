fb8e9476ab378e27d0ee226172c060a43cdbb6f0
David Oliver <davooliver1964@gmail.com>
2020-07-09 18:57:42 -0400

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { render } from 'react-dom';
-import { createStore } from 'redux';
-import rootReducer from './reducers/todo';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
 
-// const store = createStore(rootReducer);
+const store = createStore(rootReducer)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
